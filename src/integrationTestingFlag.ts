5c2f8f541e245ff128bd14fde449b89ee142ecd0
michaelecolley <michael.e.colley@googlemail.com>
2021-09-09 00:31:25 +0100

    Remove travis integration test flag

--- src/integrationTestingFlag.ts
+++ src/integrationTestingFlag.ts
@@ -1,10 +1,6 @@
 require('dotenv').config();
 
 function integrationTestingFlag() {
-  console.log(
-    '🚧 Travis integration Test Flag: ',
-    process.env.TRAVIS_BUILD_STAGE_NAME !== undefined
-  );
   return process.env.TRAVIS_BUILD_STAGE_NAME !== undefined;
 }
 
